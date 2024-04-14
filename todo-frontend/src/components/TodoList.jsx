22e53c0d9cd8d11bb0d5c03b9b8eb6b4f1fbbba3
AkshatJha21 <akshatj2134@gmail.com>
2024-04-14 11:08:57 +0530

    Design complete

--- todo-frontend/src/components/TodoList.jsx
+++ todo-frontend/src/components/TodoList.jsx
@@ -1,46 +1,48 @@
 import React, { useState } from 'react'
+import './todolist.css'
 
 const TodoList = ({ todos }) => {
   const [done, setDone] = useState(false);
 
-  
   return (
-    <div>
+    <div className='list'>
         {todos.map((todo) => {
             return (
-                <>
-                    <h2>{todo.title}</h2>
-                    <p>{todo.description}</p>
-                    <button onClick={() => {
-                      setDone(true);
-                      fetch("http://localhost:3000/done" ,{
-                        method: "PUT",
-                        body: JSON.stringify({
-                          id: todo._id
-                        }),
-                        headers: {
-                          "Content-type": "application/json"
-                        }
-                      }).then(async function (res) {
-                        await res.json();
-                        console.log("Todo updated");
-                      })
-                    }}>{todo.completed == true ? 'Done' : 'Mark as done'}</button><br />
-                    <button onClick={() => {
-                      fetch("http://localhost:3000/remove" ,{
-                        method: "DELETE",
-                        body: JSON.stringify({
-                          id: todo._id
-                        }),
-                        headers: {
-                          "Content-type": "application/json"
-                        }
-                      }).then(async function (res) {
-                        await res.json();
-                        console.log("Todo deleted");
-                      })
-                    }}>Delete</button>
-                </>
+                <div className='todo'>
+                    <h2 className='title'>{todo.title}</h2>
+                    <p className='description'>{todo.description}</p>
+                    <div className='btns'>
+                      <button className='done' onClick={() => {
+                        setDone(true);
+                        fetch("http://localhost:3000/done" ,{
+                          method: "PUT",
+                          body: JSON.stringify({
+                            id: todo._id
+                          }),
+                          headers: {
+                            "Content-type": "application/json"
+                          }
+                        }).then(async function (res) {
+                          await res.json();
+                          console.log("Todo updated");
+                        })
+                      }}>{todo.completed == true ? 'Done' : 'Mark as done'}</button>
+                      <button className='delete' onClick={() => {
+                        fetch("http://localhost:3000/remove" ,{
+                          method: "DELETE",
+                          body: JSON.stringify({
+                            id: todo._id
+                          }),
+                          headers: {
+                            "Content-type": "application/json"
+                          }
+                        }).then(async function (res) {
+                          await res.json();
+                          console.log("Todo deleted");
+                        })
+                      }}>Delete</button>
+                    </div>
+                </div>
             )
         })}
     </div>
